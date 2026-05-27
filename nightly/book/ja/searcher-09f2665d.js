b21b7116c484cc76f51c6b79f24256b88e77a58c
dalance <dalance@users.noreply.github.com>
2026-05-27 06:26:14 +0000

    deploy: c3b01bb33092a81df0fa51dd2d49496faa163116

--- nightly/book/ja/searcher-09f2665d.js
+++ nightly/book/ja/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2c9cf7bf.js',
+                path_to_root + 'searchindex-930c7e49.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- nightly/book/searcher-09f2665d.js
+++ nightly/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b800ec7e.js',
+                path_to_root + 'searchindex-b3055dba.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
