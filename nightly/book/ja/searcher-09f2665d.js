11313b7928c01229f2924a04a99b4b5a7a44d163
dalance <dalance@users.noreply.github.com>
2026-08-24 08:49:48 +0000

    deploy: 2514723ef51096027b85e397956ba35bc0b6d5e3

--- nightly/book/ja/searcher-09f2665d.js
+++ nightly/book/ja/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d07c883f.js',
+                path_to_root + 'searchindex-4aa4db1c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
