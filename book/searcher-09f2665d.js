104170a98941ded85b1733834e7ca35c1ef97fd7
dalance <dalance@users.noreply.github.com>
2026-09-02 01:57:37 +0000

    deploy: 29c83cc5d5afaa24187564271344069d9368ba18

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-35ef2795.js',
+                path_to_root + 'searchindex-b1ef2cf3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
