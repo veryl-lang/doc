15a9988ab34655a1b4c6bd2beafa689aa133992f
dalance <dalance@users.noreply.github.com>
2026-09-05 02:35:15 +0000

    deploy: 03d5f8ce21433a5691f99d96f6794142f0d600c1

--- nightly/playground/pkg/veryl_wasm.js
+++ nightly/playground/pkg/veryl_wasm.js
@@ -236,7 +236,7 @@ export function simulate(source) {
 function __wbg_get_imports() {
     const import0 = {
         __proto__: null,
-        __wbg___wbindgen_throw_bb96b2010945f0bc: function(arg0, arg1) {
+        __wbg___wbindgen_throw_5d9e815e6fdf150f: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
         __wbg_error_757e9472f8410341: function(arg0, arg1) {
